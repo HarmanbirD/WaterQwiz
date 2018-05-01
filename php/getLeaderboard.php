cfa3c7928e5edad219f469fc425f2dc6048d14cd
Jacob Smith <jacob13smith@gmail.com>
2018-05-01 15:50:04 -0700

    fixed leaderboard getter

---
+++ php/getLeaderboard.php
@@ -0,0 +1,22 @@
+<?php
+
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$dbName = "WaterQwiz";
+
+$mysqli = new mysqli($serverName, $userName, $password, $dbName);
+
+$tempArray = array();
+if ($result = $mysqli->query("SELECT * FROM Leaderboard ORDER BY score DESC")) {
+
+	while($row = $result->fetch_array(MYSQL_ASSOC)) {
+		$myArray[] = $row;
+	}
+	echo json_encode($myArray);
+}
+
+$result->close();
+$mysqli->close();
+
+?>

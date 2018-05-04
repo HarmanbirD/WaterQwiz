33123118b4202511bfa2e4f6fc1ab86a92418724
Jacob Smith <jacob13smith@gmail.com>
2018-05-03 19:51:24 -0700

    fml

--- php/getQuestions.php
+++ php/getQuestions.php
@@ -1,6 +1,7 @@
 <?php
 
 $serverName = "tcp:waterqwiz.database.windows.net, 1433";
+
 $connectionOptions = array("Database" => "WaterQwiz",
                           "UID" => "waterqwiz@waterqwiz",
                           "PWD" => "BCIT_Jacob");
@@ -19,13 +20,12 @@ if($result === false) {
     die( print_r( sqlsrv_errors(), true) );
 }
 
-while($row = fetch_array($result,SQLSRV_FETCH_ASSOC)) {
+while($row = sqlsrv_fetch_array($result,SQLSRV_FETCH_ASSOC)) {
 	$tempArray[] = $row;
 }
 echo json_encode($myArray);
 
 
 sqlsrv_free_stmt($result);
-
 
 ?>

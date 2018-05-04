2d47e521375dd0b658e3fc0e9ac19d2232a47a7a
Jacob Smith <jacob13smith@gmail.com>
2018-05-03 18:13:40 -0700

    trying to fix database

--- php/getLeaderboard.php
+++ php/getLeaderboard.php
@@ -7,7 +7,7 @@ $dbName = "WaterQwiz";
 $mysqli = new mysqli($serverName, $userName, $password, $dbName);
 
 $tempArray = array();
-if ($result = $mysqli->query("SELECT name, score FROM Leaderboard ORDER BY score DESC")) {
+if ($result = $mysqli->query("SELECT name, score FROM leaderboard ORDER BY score DESC")) {
 
 	while($row = $result->fetch_array(MYSQL_ASSOC)) {
 		$myArray[] = $row;

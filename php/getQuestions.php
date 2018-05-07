773c472428eceeb807bdc32e9d0ce7b4cf1da40c
Jacob Smith <jacob13smith@gmail.com>
2018-05-07 15:17:19 -0700

    add error log

--- php/getQuestions.php
+++ php/getQuestions.php
@@ -15,15 +15,15 @@ if($conn === false)
 
 $tempArray = array();
 $result = sqlsrv_query($conn, "SELECT * FROM questions;");
+
 if($result === false) {
     die( print_r( sqlsrv_errors(), true) );
 }
 
-while($row = sqlsrv_fetch_array($result,SQLSRV_FETCH_ASSOC)) {
+while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
 	$tempArray[] = $row;
 }
 
-error_log("JSON Object returned: " . json_encode($tempArray));
 echo json_encode($tempArray);
 
 sqlsrv_free_stmt($result);

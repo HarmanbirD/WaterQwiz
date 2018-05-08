301309a356d991b3f1f0ddd5bb2aadc5421ce1d2
Harmanbir Dhillon <harmanbir_19@hotmail.com>
2018-05-08 14:08:37 -0700

    added js to submit to leaderboard

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -129,7 +129,7 @@ $(document).ready(function() {
             }
              
             function endGame() {
-                document.getElementById('questions').innerHTML = "<div id='popop'>Game Over!</div><div id = 'score'>Your score is "  + score + " out of " + numberOfQuestions + "<div class='form-group'><label for='usr'>Name:</label><input type='text' id = 'sendName' class='form-control' placeholder = 'e.g. Jacob Smith' id='endgamename'></div><button type='button' onclick = 'sendName()' class='btn btn-info' value='Submit Button'></button>";
+                document.getElementById('questions').innerHTML = "<div id='popop'>Game Over!</div><div id = 'score'>Your score is "  + score + " out of " + numberOfQuestions + "<div class='form-group'><label for='usr'>Name:</label><input type='text' id = 'sendName' class='form-control' placeholder = 'e.g. Jacob Smith' id='endgamename'></div><button type='button' onclick = 'sendName()' class='btn btn-info' value='Submit Button'>Submit</button>";
             }
 
             function outOfTime(){

c6f5342ac2a0ac81643457b7489cec340a7a47d6
Harmanbir Dhillon <harmanbir_19@hotmail.com>
2018-05-08 13:10:58 -0700

    added js to submit to leaderboard

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -125,7 +125,16 @@ $(document).ready(function() {
             }
              
             function endGame() {
-                document.getElementById('questions').innerHTML = "<div id='popop'>Game Over!</div><div id = 'score'>Your score is "  + score + " out of " + numberOfQuestions + "<div class='form-group'><label for='usr'>Name:</label><input type='text' class='form-control' id='name'></div><input type='submit' class='btn btn-info' value='Submit Button'>";
+                document.getElementById('questions').innerHTML = "<div id='popop'>Game Over!</div><div id = 'score'>Your score is "  + score + " out of " + numberOfQuestions + "<div class='form-group'><label for='usr'>Name:</label><input type='text' id = 'sendName' class='form-control' placeholder = 'e.g. Jacob Smith' id='endgamename'></div><input type='submit' onclick = 'sendName()' class='btn btn-info' value='Submit Button'>";
+            }
+            
+            function sendName() {
+                var name = document.getElementById("sendName").value;
+                $.ajax({
+                    type: "POST",
+                     url: "../php/addScore.php",
+                    data: {name : name, score : score}
+                });
             }
 
                 })

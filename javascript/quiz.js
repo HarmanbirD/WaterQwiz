85cab4566601bde829049f67cda29aa91d514912
Harmanbir Dhillon <harmanbir_19@hotmail.com>
2018-05-08 13:51:18 -0700

    added js to submit to leaderboard

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -130,8 +130,18 @@ $(document).ready(function() {
             }
              
             function endGame() {
-                document.getElementById('questions').innerHTML = "<div id='popop'>Game Over!</div><div id = 'score'>Your score is "  + score + " out of " + numberOfQuestions + "<div class='form-group'><label for='usr'>Name:</label><input type='text' id = 'sendName' class='form-control' placeholder = 'e.g. Jacob Smith' id='endgamename'></div><input type='submit' onclick = 'sendName()' class='btn btn-info' value='Submit Button'>";
+                document.getElementById('questions').innerHTML = "<div id='popop'>Game Over!</div><div id = 'score'>Your score is "  + score + " out of " + numberOfQuestions + "<div class='form-group'><label for='usr'>Name:</label><input type='text' id = 'sendName' class='form-control' placeholder = 'e.g. Jacob Smith' id='endgamename'></div><div class = 'ends'><input type='submit' class='btn btn-info' value='Submit Button'></div>";
             }
+            
+            $('.ends').click(function() {
+                var name = document.getElementById("sendName").value;
+                $.ajax({
+                    type: "POST",
+                     url: "../php/addScore.php",
+                    data: {name : name, score : score}
+                });
+
+            })
 
             function outOfTime(){
                 document.getElementById("howTo").setAttribute('disabled',false);

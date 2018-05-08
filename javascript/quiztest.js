b7a3619ebd10757edb82966c51a6cc4748d0fade
jonchiu94 <jchiu84@my.bcit.ca>
2018-05-08 14:13:22 -0700

    changed timer

--- javascript/quiztest.js
+++ javascript/quiztest.js
@@ -6,8 +6,25 @@ $(document).ready(function() {
         questionLock = false,
         numberOfQuestions,
         score = 0,
-        reset = false;
-  
+        running = true;
+    function startTimer(duration) {
+                var running = true;
+                var timer = duration, seconds
+                    setInterval(function () {
+                        if (running){
+                            seconds = parseInt(timer % 60, 10);
+                            seconds = seconds < 10 ? "0" + seconds : seconds;
+                            gauge.set(timer);
+                            timer--;
+                            if (timer < 0) {
+                                endGame();
+                            }
+                        }
+                    }, 1000);
+                }
+    function endGame() {
+        document.getElementById('questions').innerHTML = "<div id='popop'>Game Over!</div><div id = 'score'>Your score is "  + score + " out of " + numberOfQuestions + "<div class='form-group'><label for='usr'>Name:</label><input type='text' id = 'sendName' class='form-control' placeholder = 'e.g. Jacob Smith' id='endgamename'></div><input type='submit' onclick = 'sendName()' class='btn btn-info' value='Submit Button'>";
+    }
     var opts = {
         angle: 0, // The span of the gauge arc
         lineWidth: 0.15, // The line thickness
@@ -30,7 +47,8 @@ $(document).ready(function() {
         generateGradient: true,
         highDpiSupport: true,     // High resolution support
     };
-                        
+    //start timer
+    startTimer(100)            
     var target = document.getElementById('waterMeter'); // your canvas element
     var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
     gauge.maxValue = 100; // set max gauge value
@@ -99,8 +117,6 @@ $(document).ready(function() {
                 q2=questionBank[questionNumber][2];
                 q3=questionBank[questionNumber][3];
             }
-            var display = document.querySelector('#timer');
-            startTimer(10, display);
             $(stage).append('<div class = "score">Score: '+score+' / ' +questionNumber+ '</div><div class="questionText">'+questionBank[questionNumber][0]+'</div><div id="1" class="option"><button type="button" id = "btn-1" class="btn btn-default btn-lg">'+q1+'</button></div><div id="2" class="option"><button type="button" id = "btn-2" class="btn btn-default btn-lg">'+q2+'</button></div><div id="3" class="option"><button type="button" id = "btn-3" class="btn btn-default btn-lg">'+q3+'</button></div><div id="4" class="option"><button type="button" id = "btn-4" class="btn btn-default btn-lg">'+q4+'</button></div>');
             $(stage).css("right","-1000px");
             $(stage).animate({opacity: "1"}, {duration: 1000, queue: false});
@@ -137,7 +153,6 @@ $(document).ready(function() {
             })
         
             function changeQuestion() { 
-                reset = true;
                 document.getElementById('popop').innerHTML = "";
                 questionNumber++;
                 if(questionNumber<numberOfQuestions) {
@@ -150,10 +165,6 @@ $(document).ready(function() {
                     endGame();
                 }
             }
-             
-            function endGame() {
-                document.getElementById('questions').innerHTML = "<div id='popop'>Game Over!</div><div id = 'score'>Your score is "  + score + " out of " + numberOfQuestions + "<div class='form-group'><label for='usr'>Name:</label><input type='text' id = 'sendName' class='form-control' placeholder = 'e.g. Jacob Smith' id='endgamename'></div><input type='submit' onclick = 'sendName()' class='btn btn-info' value='Submit Button'>";
-            }
         
             })
     
@@ -165,36 +176,6 @@ $(document).ready(function() {
                     data: {name : name, score : score}
                 });
             }
-            function outOfTime(){
-                document.getElementById("howTo").setAttribute('disabled',false);
-                            $("#bg").css('filter', 'blur(1px)');
-                            $("body").css('box-shadow', 'inset 0px 0px 400px 110px rgba(0, 0, 0, .7)');
-                            $(".option").css('filter', 'brightness(80%)');
-                            $(mainStage).append('<div class = "modal-dialog" id="popup"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">Out of Time!</h4></div><div class="modal-body">'+questionBank[questionNumber][5]+'</div><div class="modal-footer"><button type="button" id="next-question" class="btn btn-default btn-lg">Next question</button></div></div></div>');
-            }
-            function startTimer(duration, display) {
-                var running = true;
-                var timer = duration, seconds
-                    setInterval(function () {
-                        if (running){
-                            seconds = parseInt(timer % 60, 10);
-                            seconds = seconds < 10 ? "0" + seconds : seconds;
-                            display.textContent = seconds;
-                            timer--;
-                            if (timer < 0) {
-                                outOfTime();
-                                timer = duration;
-                                running = false;
-                            }
-                            else if(reset){
-                                timer = duration;
-                                running = false;
-                                reset = false;
-                            }
-                        }
-                    }, 1000);
-                }
-            
                 
             window.onbeforeunload = function(evt){
                 if (typeof evt == 'undefined') {

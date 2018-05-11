644192d959d8d1dfeb89f611dd40f4a38769a0ac
Harmanbir Dhillon <harmanbir_19@hotmail.com>
2018-05-11 13:25:03 -0700

    fixed easter egg finally

--- javascript/easterEgg.js
+++ javascript/easterEgg.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
     document.getElementById ("banner").addEventListener ("click", updateCounter, false);
         var count = 0;
         function updateCounter() {
+            count++;  
             if (count % 10 == 0 && count != 0) {
                 document.getElementById("bannerIndex").innerHTML = "<img id='banner' src='./images/banner-lg-new2.0.png' alt='Banner'>";
             }
-            count++;  
             if (count == 9) {
                 document.getElementById("bannerIndex").innerHTML = "<a href='https://jacobsmith.xyz/' target='whatever'><img id='banner' src='./images/banner-lg-new2.0.png' alt='Banner'></a>";
                 count = 0;

71a568992e3081c28769ef91603fce4610b82868
hsyuting <hsyuting@gmail.com>
2018-05-11 18:29:04 -0700

    change back to not test.js

--- javascript/leaderboard.js
+++ javascript/leaderboard.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
     $('#leaderboard-modal').on('show.bs.modal', function(e) {
-        document.getElementById("leaders").innerHTML = "<div id = 'gifDiv'><img id = 'loadingGif' src = './images/watertap.gif'></div>";
+        //document.getElementById("leaders").innerHTML = "<div id = 'gifDiv'><img id = 'loadingGif' src = './images/watertap.gif'></div>";
     
         $.ajax({
             dataType: "json",

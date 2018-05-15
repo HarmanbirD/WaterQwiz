e3b6946fef50565c69f76fdca8ac02e0e28708c4
Harmanbir Dhillon <harmanbir_19@hotmail.com>
2018-05-15 09:46:51 -0700

    fixed end game button styling

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -224,7 +224,7 @@ function sendName() {
             document.getElementById("howTo").setAttribute('disabled',false);
             $("body").css('box-shadow', 'inset 0px 0px 400px 110px rgba(0, 0, 0, .7)');
             //$(".option").css('filter', 'brightness(80%)');
-            $(mainStage).append('<div class = "modal-dialog" id="leaderboard-modal"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">Leaderboard</h4></div><div id = "leaders" class="modal-body">There was an error loading the leaderboard. Sorry.</div><div class="modal-footer"><div class = "modal-footer-spacing"><div class = "col-xs-6"><img id = "modal-drippy" src = "../images/points-drippy.png"></div><div class = "col-xs-6"><button type="button" class="btn btn-danger" onclick = "location.href = \'../index.html\'" data-dismiss="modal">Close</button></div></div>');
+            $(mainStage).append('<div class = "modal-dialog" id="leaderboard-modal"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">Leaderboard</h4></div><div id = "leaders" class="modal-body">There was an error loading the leaderboard. Sorry.</div><div class="modal-footer"><div class = "modal-footer-spacing"><div class = "col-xs-6"><img id = "modal-drippy" src = "../images/points-drippy.png"></div><div class = "col-xs-6"><button type="button" class="btn btn-danger" id = "endGameBTN" onclick = "location.href = \'../index.html\'" data-dismiss="modal">Close</button></div></div>');
             document.getElementById("leaders").innerHTML = code;
             document.getElementById("submitBut").setAttribute('disabled',false);
                 })
